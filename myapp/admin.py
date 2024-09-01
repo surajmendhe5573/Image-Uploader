24019c7cca83ce69b19b9866113b1421e0c794bf
surajmendhe5573 <surajmendhe470@gmail.com>
2024-09-01 15:35:23 +0530

    first commit

---
+++ myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Image
+
+# Register your models here.
+
+@admin.register(Image)
+class ImaageAdmin(admin.ModelAdmin):
+    list_display= ['id', 'photo', 'date']
